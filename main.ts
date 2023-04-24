567960ba2a2bfbb6fc06b928db2e710fde200ad6
manuelcasas <128051762+manuelcasas23@users.noreply.github.com>
2023-04-24 08:59:24 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,45 @@
+basic.forever(function () {
+    if (input.lightLevel() <= 50) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            `)
+    } else if (input.lightLevel() >= 51 && input.lightLevel() <= 100) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            # # # # #
+            `)
+    } else if (input.lightLevel() >= 101 && input.lightLevel() <= 150) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else if (input.lightLevel() >= 151 && input.lightLevel() <= 200) {
+        basic.showLeds(`
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else if (input.lightLevel() >= 201 && input.lightLevel() <= 255) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else {
+        basic.clearScreen()
+    }
+})
